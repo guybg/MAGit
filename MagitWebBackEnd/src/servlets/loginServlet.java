e445dcfc0915ada8eb336dae747406bd224a78fe
guybg <guybbe@gmail.com>
2019-09-20 23:54:08 +0300

    part3 starter

--- MagitWebBackEnd/src/servlets/loginServlet.java
+++ MagitWebBackEnd/src/servlets/loginServlet.java
@@ -35,7 +35,7 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String usernameFromSession = SessionUtils.getUsername(request);
-       // UserManager userManager = ServletUtils.getUserManager(getServletContext());
+        // UserManager userManager = ServletUtils.getUserManager(getServletContext());
         if (usernameFromSession == null) {
             //user is not logged in yet
             String usernameFromParameter = request.getParameter(USERNAME);
@@ -60,30 +60,30 @@ public class LoginServlet extends HttpServlet {
                 A better code would be to perform only as little and as necessary things we need here inside the synchronized block and avoid
                 do here other not related actions (such as request dispatcher\redirection etc. this is shown here in that manner just to stress this issue
                  */
-            //   synchronized (this) {
-            //       if (userManager.isUserExists(usernameFromParameter)) {
-            //           String errorMessage = "Username " + usernameFromParameter + " already exists. Please enter a different username.";
-            //           // username already exists, forward the request back to index.jsp
-            //           // with a parameter that indicates that an error should be displayed
-            //           // the request dispatcher obtained from the servlet context is one that MUST get an absolute path (starting with'/')
-            //           // and is relative to the web app root
-            //           // see this link for more details:
-            //           // http://timjansen.github.io/jarfiller/guide/servlet25/requestdispatcher.xhtml
-            //           request.setAttribute(Constants.USER_NAME_ERROR, errorMessage);
-            //           getServletContext().getRequestDispatcher(LOGIN_ERROR_URL).forward(request, response);
-            //       } else {
-            //           //add the new user to the users list
-            //           userManager.addUser(usernameFromParameter);
-            //           //set the username in a session so it will be available on each request
-            //           //the true parameter means that if a session object does not exists yet
-            //           //create a new one
-            //           request.getSession(true).setAttribute(Constants.USERNAME, usernameFromParameter);
+                //   synchronized (this) {
+                //       if (userManager.isUserExists(usernameFromParameter)) {
+                //           String errorMessage = "Username " + usernameFromParameter + " already exists. Please enter a different username.";
+                //           // username already exists, forward the request back to index.jsp
+                //           // with a parameter that indicates that an error should be displayed
+                //           // the request dispatcher obtained from the servlet context is one that MUST get an absolute path (starting with'/')
+                //           // and is relative to the web app root
+                //           // see this link for more details:
+                //           // http://timjansen.github.io/jarfiller/guide/servlet25/requestdispatcher.xhtml
+                //           request.setAttribute(Constants.USER_NAME_ERROR, errorMessage);
+                //           getServletContext().getRequestDispatcher(LOGIN_ERROR_URL).forward(request, response);
+                //       } else {
+                //           //add the new user to the users list
+                //           userManager.addUser(usernameFromParameter);
+                //           //set the username in a session so it will be available on each request
+                //           //the true parameter means that if a session object does not exists yet
+                //           //create a new one
+                //           request.getSession(true).setAttribute(Constants.USERNAME, usernameFromParameter);
 
-            //           //redirect the request to the chat room - in order to actually change the URL
-            //           System.out.println("On login, request URI is: " + request.getRequestURI());
-            //           response.sendRedirect(CHAT_ROOM_URL);
-            //       }
-            //   }
+                //           //redirect the request to the chat room - in order to actually change the URL
+                //           System.out.println("On login, request URI is: " + request.getRequestURI());
+                //           response.sendRedirect(CHAT_ROOM_URL);
+                //       }
+                //   }
             }
         } else {
             //user is already logged in
