283c47dea9b21bb1f3f4bd1b3115e086783e8b5e
guybg <guybbe@gmail.com>
2019-09-23 19:01:38 +0300

    new repository from xml implementation. continued

--- MagitWebBackEnd/web/pages/mainScreen/mainScreen.js
+++ MagitWebBackEnd/web/pages/mainScreen/mainScreen.js
@@ -1,9 +1,10 @@
 var USER_DETAILS_URL = buildUrlWithContextPath("details");
 var UPLOAD_URL = buildUrlWithContextPath("upload");
+var refreshRate = 2000;
 var h = document.cookie;
 var accountDetails;
-// dataJson = {color: "" ; height: ""}
-function createRepository(location, name){
+// {"userName":"gh","repositories":{"banana":{"commitMessage":"msg..","name":"repo name","commitDate":"5/5/15","branchesNum":"5","activeBranch":"branch"}, ..}
+    function createRepository(name, details){
     var repository = $("<div class=\"col-xl-3 col-sm-6 mb-3\">" +
         "<div class=\"card text-white bg-primary o-hidden h-100\">" +
         "<div class=\"card-body\">" +
@@ -11,7 +12,19 @@ function createRepository(location, name){
         "<i class=\"fas fa-fw fa-comments\"></i>" +
         "</div>" +
         "<div class=\"mr-5\">" +
-        name +
+        "Repository: "  + details.name +
+        "</div>" +
+        "<div class=\"mr-5\">" +
+        "Active branch: " + details.activeBranch +
+        "</div>" +
+        "<div class=\"mr-5\">" +
+        "Branches: " + details.branchesNum +
+        "</div>" +
+        "<div class=\"mr-5\">" +
+        "Commit date: " + details.commitDate +
+        "</div>" +
+        "<div class=\"mr-5\">" +
+        "Commit message: " + details.commitMessage +
         "</div>" +
         "</div>" +
         "<a class=\"card-footer text-white clearfix small z-1\" href=\"#\">" +
@@ -73,12 +86,11 @@ function bs_input_file() {
     );
 }
 $(function () {
-    $("#repositoriesbutton").click(showRepositories);
+    $("#repositoriesbutton").click(showRepositoriesPage);
     $("#logout").click(logout);
-    bs_input_file();
 })
 
-$(function() { // onload...do
+function uploadAjaxSubmit() {
     $("#uploadForm").submit(function () {
 
         var file = this[0].files[0];
@@ -103,7 +115,7 @@ $(function() { // onload...do
         });
         return false;
     });
-});
+};
 
 function updateInputLabel() {
     //get the file name
@@ -122,35 +134,62 @@ function showRepositories() {
         success: function(msg) {
             //{"userName":"Guy","repositories":{}}
             $("#repository-container").empty();
-            $("#repository-upload").empty();
-            $("#repository-upload").append(
-                '<div class="col-xl-6 col-sm-8 mb-3">' +
-                    '<form class="md-form" id="uploadForm" action="/upload" enctype="multipart/form-data" method="POST">'+
-                        '<div class="input-group">' +
-                            '<div class="input-group-prepend">' +
-                                '<span class="input-group-text" id="inputGroupFileAddon01">Upload repository</span>' +
-                            '</div>' +
-                            '<div class="custom-file">' +
-                                '<input type="file" class="custom-file-input" id="inputGroupFile01"' +
-                                'aria-describedby="inputGroupFileAddon01">' +
-                            '<form>' +
-                                '<label class="custom-file-label" for="inputGroupFile01">Choose file</label>' +
-                            '</form>' +
-                            '</div>' +
-                            '</div>' +
-                            '<div class="file-path-wrapper">' +
-                            '<input type="Submit" value="Upload File" class="btn btn-primary">' +
-                '</div>' +
 
-                '   </form>' +
-                '</div>');
-            $("#inputGroupFile01").change(updateInputLabel);
+                //'<div class="col-xl-6 col-sm-8 mb-3">' +
+                //    '<form class="md-form" id="uploadForm" action="/upload" enctype="multipart/form-data" method="POST">'+
+                //        '<div class="input-group">' +
+                //            '<div class="input-group-prepend">' +
+                //                '<span class="input-group-text" id="inputGroupFileAddon01">Upload repository</span>' +
+                //            '</div>' +
+                //            '<div class="custom-file">' +
+                //                '<input type="file" class="custom-file-input" id="inputGroupFile01"' +
+                //                'aria-describedby="inputGroupFileAddon01">' +
+                //            '<form>' +
+                //                '<label class="custom-file-label" for="inputGroupFile01">Choose file</label>' +
+                //            '</form>' +
+                //            '</div>' +
+                //            '</div>' +
+                //            '<div class="file-path-wrapper">' +
+                //            '<input type="Submit" value="Upload File" class="btn btn-primary">' +
+                //'</div>' +
+//
+                //'   </form>' +
+                //'</div>');
+
             var repositories = msg.repositories;
             $.each(repositories || [], createRepository);
         }
     });
 }
 
+function showRepositoriesPage() {
+    showRepositories();
+    $("#repository-upload").empty();
+    $("#repository-upload").append(
+        '<div class="col-xl-3 col-sm-6 mb-3">'+
+        '<form id="uploadForm" method="POST" action="/upload" enctype="multipart/form-data">'+
+        <!-- COMPONENT START -->
+        '<div class="form-group">'+
+        '<div class="input-group input-file" name="Fichier1">' +
+        '<input type="text" class="form-control" placeholder="Choose a file..." />'+
+        '<span class="input-group-btn">'+
+        '<button class="btn btn-secondary btn-choose" type="button">Choose</button>'+
+        '</span>'+
+        '</div>'+
+        '</div>'+
+        <!-- COMPONENT END -->
+        '<div class="form-group">'+
+        '<button type="submit" class="btn btn-primary pull-right">Submit</button>'+
+        '&nbsp;&nbsp;&nbsp;' +
+        '<button type="reset" class="btn btn-danger">Reset</button>'+
+        '</div>'+
+        '</form>'+
+        '</div>');
+    bs_input_file();
+    uploadAjaxSubmit();
+    $("#inputGroupFile01").change(updateInputLabel);
+    setInterval(showRepositories, refreshRate);
+}
 
 function logout() {
     var LOUGOUT_URL = buildUrlWithContextPath("/pages/signup/logout");
